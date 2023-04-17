3259bde7b95267c1ff3cc5524c8f2a4b42aeca53
Jayden <hello.jayden@protonmail.com>
2023-04-17 18:30:37 +1000

    new page template test

--- questfrens-website/src/components/verify.js
+++
@@ -1,5 +0,0 @@
-export default function verify(address, signature) {
-    const bitcoinMessage = require('bitcoinjs-message')
-
-    console.log(bitcoinMessage.verify(address, address, signature))
-}
